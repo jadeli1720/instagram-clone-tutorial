6b8484d7c396567d4a3f13c7984b8757c53086d2
jadeli1720 <jadeli1720@gmail.com>
2021-09-21 16:59:42 -0600

    Starting to set up routes and constants

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+const Login = lazy(()=> import ('./pages/login'))
 
 function App() {
   return (
-    <p>Hello!</p>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>} >
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
