b4599498848dce6d39f648f4fecda6b094fd558a
jadeli1720 <jadeli1720@gmail.com>
2021-09-21 17:13:35 -0600

    importing useState, useEffect, and useContext

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,24 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+
 export default function Login(){
+    const [ emailAddress, setEmailAddress ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ error, setError ] = useState('');
+ 
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext)
+
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleLogin = () => {}
+
+    useEffect(() => {
+        document.title = "Login - Instagram"
+    }, [])
+
     return(
         <p>I am the login page</p>
     )
