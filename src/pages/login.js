57c294202510f865040f77d8d9b81d6c14918f87
jadeli1720 <jadeli1720@gmail.com>
2021-09-21 22:46:33 -0600

    Added dependecies and added configurations to set up tailwind

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,6 +20,13 @@ export default function Login(){
     }, [])
 
     return(
-        <p>I am the login page</p>
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <div className="flex w-3/5">
+                <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+            </div>
+            <div className="flex flex-col w-2/5">
+                <p>I will be the form</p>
+            </div>
+        </div>
     )
 }
