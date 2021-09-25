80f6439f795e8e78fe481ca8e60bbd904fb74fa4
jadeli1720 <jadeli1720@gmail.com>
2021-09-24 22:29:01 -0600

    added sign up page and added value for each input for both sign up and log in pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 
-const Login = lazy(()=> import ('./pages/login'))
+const Login = lazy(()=> import ('./pages/login'));
+const SignUp = lazy(()=> import ('./pages/sign-up'))
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Suspense fallback={<p>Loading...</p>} >
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGNUP} component={SignUp} />
         </Switch>
       </Suspense>
     </Router>
