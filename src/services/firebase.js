7d0f29aaadad538996db1957c099e8fc3d283494
jadeli1720 <jadeli1720@gmail.com>
2021-09-26 20:42:48 -0600

    Setting up hooks and services to get users by their id's

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -7,5 +7,21 @@ export async function doesUsernameExist(username) {
         .where('username', '==', username)
         .get();
 
-        return result.docs.length > 0;
+    return result.docs.length > 0;
 };
+
+// get user from the firestore where userId === userId (passed from the auth)
+export async function getUserByUserId(userId) {
+    const result = await firebase
+        .firestore()
+        .collection('users')
+        .where('userId', '==', userId)
+        .get();
+
+    const user = result.docs.map((item) => ({
+        ...item.data(),
+        docId: item.id
+    }))
+    
+    return user;
+}
