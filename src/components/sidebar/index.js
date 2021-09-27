599c3ff61aeb8ad4addc7d4997066725af8b8a6c
jadeli1720 <jadeli1720@gmail.com>
2021-09-26 21:20:10 -0600

    Moving files around

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,18 @@
+import useUser from "../../hooks/use-user";
+import Users from "./user";
+import Suggestions from "./suggestions";
+
+export default function Sidebar() {
+    const { 
+        user : { fullname, username, userId, following}
+    } = useUser()
+
+    // console.log(`fullname: ${fullname}, username: ${username}, userID: ${userId} `)
+
+    return(
+        <div className="p-4" >
+            <Users username = { username } fullname = { fullname } />
+            <Suggestions userId = { userId } following={following} />
+        </div>
+    )
+}
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Header from '../components/header';
 import Timeline from '../components/timeline';
-import Sidebar from '../components/sidebar/sidebar';
+import Sidebar from '../components/sidebar';
 
 export default function Dashboard() {
     useEffect(() => {
