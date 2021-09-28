f296fa7b0286931f54ca3d9fc47bbabff88a8e25
jadeli1720 <jadeli1720@gmail.com>
2021-09-27 23:08:18 -0600

    Have pictures for suggested people to follow.

--- src/components/sidebar/suggested-profile.js
+++ src/components/sidebar/suggested-profile.js
@@ -1,18 +1,37 @@
-import { useContext, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { useContext, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function SuggestedProfile({
-    profileDocId,
-    username,
-    profileId,
-    userId
+	profileDocId,
+	username,
+	profileId,
+	userId,
 }) {
-    const [followed, setFollowed] = useState(false);
+	const [followed, setFollowed] = useState(false);
 
-    return !followed ? (
-        <div className = "flex flex-row items-center align-items justify-between ">
-
-        </div>
-    ) : null;
+	return !followed ? (
+		<div className="flex flex-row items-center align-items justify-between ">
+			<div className="flex items-center justify-between">
+				<img
+					className="rounded-full w-8 flex mr-3"
+					src={`/images/avatars/${username}.jpg`}
+					alt=""
+					onError={(e) => {
+						e.target.src = `/images/avatars/default.png`;
+					}}
+				/>
+				<Link to={`/p/${username}`}>
+					<p className="font-bold text-sm">{username}</p>
+				</Link>
+			</div>
+			<button
+				className="text-xs font-bold text-blue-medium"
+				type="button"
+				// onClick={handleFollowUser}
+			>
+				Follow
+			</button>
+		</div>
+	) : null;
 }
