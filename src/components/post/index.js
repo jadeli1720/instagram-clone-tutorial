1d97443e917bdbe1e73d7e9bfa1b4589840d366a
jadeli1720 <jadeli1720@gmail.com>
2021-10-11 23:06:02 -0600

    Added post Header and Image components

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,8 +1,15 @@
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
+import Header from './header';
+import Image from './image';
 
 export default function Post({ content }) {
-    return <p>I am the Post component</p>
+    return (
+        <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
+            <Header username={content.username}/>
+            <Image src={content.imageSrc} caption={content.caption}/>
+        </div>
+    )
 }
 
 Post.propTypes = {
